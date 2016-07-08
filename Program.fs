d5b608e1a5b677d2d50fa03cf93ed5c2301e83be
pshirshov <pshirshov@gmail.com>
2016-07-08 22:13:55 +0100

    initial demo

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+
+
+type Startup() = 
+    member this.Configure(app: IApplicationBuilder) =
+      app.Run(fun context -> context.Response.WriteAsync("Hello from ASP.NET Core!"))
+
+
+[<EntryPoint>]
+let main argv = 
+    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
+    host.Run()
+    printfn "Server finished!"
+    0
