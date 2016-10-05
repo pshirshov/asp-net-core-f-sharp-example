88983780f11c7138ce746ec96fb75cc175361c28
Kevin Hoffman <kehoffman@pivotal.io>
2016-10-05 08:26:02 -0400

    Updating so this can accept port binding from command line

--- Program.fs
+++ Program.fs
@@ -1,18 +1,21 @@
-﻿open System
+open System
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Configuration.CommandLine
 
 
-type Startup() = 
+type Startup() =
     member this.Configure(app: IApplicationBuilder) =
       app.Run(fun context -> context.Response.WriteAsync("Hello from ASP.NET Core!"))
 
 
 [<EntryPoint>]
-let main argv = 
-    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
+let main argv =
+    let config = ConfigurationBuilder().AddCommandLine(argv).Build()
+    let host = WebHostBuilder().UseKestrel().UseConfiguration(config).UseStartup<Startup>().Build()
     host.Run()
     printfn "Server finished!"
     0
